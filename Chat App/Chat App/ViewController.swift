3f2f4dc082712196ce4c0332c47a99ce7bdcfd79
FabrizioPastor <fabriziopastors@outlook.com>
2022-10-07 23:03:44 -0500

    Commit Inicial

---
+++ Chat App/Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by FABRIZIO on 7/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
